821f86d734aa9378bdf012f69c13e43f27db161b
dvbl12k <102505276+dvbl12k@users.noreply.github.com>
2022-03-28 02:06:46 -0400

    index.js
    test

---
+++ index.js
@@ -0,0 +1,17 @@
+var array = [2, 5, 9];
+array.indexOf(2);     // 0
+array.indexOf(7);     // -1
+array.indexOf(9, 2);  // 2
+array.indexOf(2, -1); // -1
+array.indexOf(2, -3); // 0
+var indices = [];
+var array = ['a', 'b', 'a', 'c', 'a', 'd'];
+var element = 'a';
+var idx = array.indexOf(element);
+while (idx != -1) {
+  indices.push(idx);
+  idx = array.indexOf(element, idx + 1);
+}
+
+console.log(indices);
+// [0, 2, 4]
